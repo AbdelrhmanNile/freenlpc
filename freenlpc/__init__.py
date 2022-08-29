7c3a132c8c72ba5755f5bb99d8c17fc2b8f1a226
AbdelrhmanNile <abdelrhmannile@gmail.com>
2022-08-29 12:30:53 +0200

    python package setup

---
+++ freenlpc/__init__.py
@@ -0,0 +1,1 @@
+from freenlpc import FreeNlpc
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'A wrapper for nlpcloud free-tier services with no requests per minute limits'
+
+# Setting up
+setup(
+    name="freenlpc",
+    version=VERSION,
+    author="Abdelrhman Nile",
+    author_email="<abdelrhmannile@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['nlpcloud'],
+    keywords=['python', 'natural languge processing', 'nlp', 'deep learning', 'AI', 'GPT', 'LLMS', 'nlpcloud'],
+    classifiers=[
+        "Development Status :: Deploying",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Linux",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
