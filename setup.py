4c5157f097d97dee3b0ff7990a34c91702bb0091
AbdelrhmanNile <abdelrhmannile@gmail.com>
2022-08-29 12:51:40 +0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 VERSION = '0.0.1'
-DESCRIPTION = 'A wrapper for nlpcloud free-tier services with no requests per minute limits'
-
+DESCRIPTION = 'A wrapper for nlpcloud free-tier services with no requests per minute limits.'
+LONG_DESCRIPTION = 'A wrapper for nlpcloud free-tier services with no requests per minute limits.'
 # Setting up
 setup(
     name="freenlpc",
@@ -10,6 +10,8 @@ setup(
     author="Abdelrhman Nile",
     author_email="<abdelrhmannile@gmail.com>",
     description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
     packages=find_packages(),
     install_requires=['nlpcloud'],
     keywords=['python', 'natural languge processing', 'nlp', 'deep learning', 'AI', 'GPT', 'LLMS', 'nlpcloud'],
