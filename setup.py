1bb6be92d036c01430617e394e6d7f64c441ca14
Abdelrhman Nile <90456995+AbdelrhmanNile@users.noreply.github.com>
2022-08-29 13:44:55 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,38 @@ from setuptools import setup, find_packages
 
 VERSION = '0.0.1'
 DESCRIPTION = 'A wrapper for nlpcloud free-tier services with no requests per minute limits.'
-LONG_DESCRIPTION = 'A wrapper for nlpcloud free-tier services with no requests per minute limits.'
+LONG_DESCRIPTION = """# freenlpc
+a wrapper for nlpcloud free-tier.
+
+# FEATURES
+- wrappes all the important nlpcloud free-tier models in one object.
+- no rate limit per minute error, it will just keep on trying until it gets the response.
+- you can initialize it with more than one API token, if one reached the rate limit it will automatically switch to the other API token.
+
+# AVAILBLE TASKS
+- classification
+- dialog summary
+- entity extraction
+- question answering
+- semantic similarity
+- sentiment/emotions analysis
+- summarization
+- embeddings
+
+# INSTALLATION
+```
+pip install freenlpc
+```
+# USAGE
+```
+from freenlpc import FreeNlpc
+
+tokens = ["token1", "token2"] # your nlpcloud api token/s
+nlpc = FreeNlpc(tokens)
+
+# then use whatever task you want
+result = nlpc.sentiment_emotions("i am feelin happy")
+```"""
 # Setting up
 setup(
     name="freenlpc",
