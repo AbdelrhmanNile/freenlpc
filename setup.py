47d19bb1d1dc9d29d7fb6ead9bb3238925a2478c
Abdelrhman Nile <90456995+AbdelrhmanNile@users.noreply.github.com>
2022-08-29 23:22:52 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.5'
+VERSION = '0.0.6'
 DESCRIPTION = 'A wrapper for nlpcloud free-tier services with no requests per minute limits.'
 LONG_DESCRIPTION = """# freenlpc
 a wrapper for nlpcloud free-tier.
